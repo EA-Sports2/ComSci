ed2ca2bd3266885d461b58fc16c13bc664b67ca1
EA-Sports2 <emaadakhtar123@gmail.com>
2026-04-13 20:40:25 +0000

    Fizzbuzz

--- program.py
+++ program.py
@@ -1,1 +1,16 @@
-print ("Hello world")
+
+def fizzbuzz():
+    x = 1
+    while (x <= 100):
+        if(x % 15 == 0):
+            print("Fizzbuzz")
+        elif(x % 3 == 0):
+            print("Fizz")
+        elif(x % 5 == 0):
+            print("buzz")
+        else:
+            print(x)   
+        x = x+1
+
+ 
+fizzbuzz()
